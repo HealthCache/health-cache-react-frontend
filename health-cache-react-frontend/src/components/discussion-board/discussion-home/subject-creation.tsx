49536418829ee8bd98fddc8a3979e79f47c173fd
Virut1316 <54875369+Virut1316@users.noreply.github.com>
2021-11-19 17:00:10 -0500

    Correction on subject creation

--- health-cache-react-frontend/src/components/discussion-board/discussion-home/subject-creation.tsx
+++ health-cache-react-frontend/src/components/discussion-board/discussion-home/subject-creation.tsx
@@ -8,46 +8,6 @@ import ModalHeader from "react-bootstrap/esm/ModalHeader";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import './subject-creation.css';
-
-
-
-export const SubjectCreation : React.FC<any> = () => {
-    const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  let counter = 0; 
-
-  const [convertedText, setConvertedText] = useState("Some default content");
-
-    useEffect(()=>{
-        counter++;
-        console.log(convertedText);
-    },[convertedText]);
-
-    return (
-        <div>
-            
-        <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-        </Button>
-      <Modal show={show} onHide={handleClose} size="lg" centered>
-          <ModalHeader>Create new subject</ModalHeader>
-            <Container>
-        < Row>
-        <Col xs={4}>4 column</Col>
-        <Col xs={4}>24 column</Col>
-        <Col xs={4}>34 column</Col>
-        </Row>
-        < Row>2</Row>
-        < Row>3</Row>
-
-            {/*Modal header*/}
-            <br/>
-            <Row>
-                <Col xs={6} md={5}>
-                <ReactQuill 
-                    id="textEditor"
-
 import './discussion.css';
 
 
