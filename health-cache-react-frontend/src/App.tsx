d237093502a93450027473b49465e11d200469f4
rimboneko <rich.mboneko@revature.net>
2021-11-19 17:59:49 -0600

    update

--- health-cache-react-frontend/src/App.tsx
+++ health-cache-react-frontend/src/App.tsx
@@ -8,7 +8,7 @@ import {Profile} from "./components/profile/Profile";
 
 import {Discussions} from "./components/discussion-board/discussion-home/Discussions";
 import {Login} from "./components/Login/Login";
-import {Register} from "./components/Register/Register";
+import {Register} from "./components/register/Register";
 import {logoutUser, User} from "./redux/actions";
 import {StoreState} from "./redux/reducers";
 import {connect, useSelector} from "react-redux";
