3dab8838a5a944deaf3c0ee271753e10a12ec970
Emanuel Pires <emanuelpires@Emanuels-Air.home>
2021-11-19 12:26:27 -0800

    modal working adjustments made to imports on Register and Loggi in app.tsk

--- health-cache-react-frontend/src/App.tsx
+++ health-cache-react-frontend/src/App.tsx
@@ -1,73 +1,71 @@
-import React, {Component, useEffect, useState} from 'react';
-import 'reset-css';
-import NavbarScroller from './components/Navbar'
-import {BrowserRouter as Router, Navigate, Route, Routes, useNavigate} from 'react-router-dom';
+import React, { Component, useEffect, useState } from "react";
+import "reset-css";
+import NavbarScroller from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
-import Claims from './components/claims/Claims';
-import {Profile} from "./components/profile/Profile";
+import Claims from "./components/claims/Claims";
+import { Profile } from "./components/profile/Profile";
 
-import {Discussions} from "./components/discussion-board/discussion-home/Discussions";
-import {Login} from "./components/login/Login";
-import {Register} from "./components/register/Register";
-import {User} from "./redux/actions";
-import {StoreState} from "./redux/reducers";
-import {connect, useSelector} from "react-redux";
-
+import { Discussions } from "./components/discussion-board/discussion-home/Discussions";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { User } from "./redux/actions";
+import { StoreState } from "./redux/reducers";
+import { connect, useSelector } from "react-redux";
 
 const navigationLogged = {
-    brandLogged: {name: 'HealthCache', to: '/'},
-    linksLogged: [
-        {name: 'Profile', to: '/Profile'},
-        {name: 'File Claim', to: '/FileClaim'},
-        {name: 'Discussion Board', to: '/Discussion'}
-
-    ]
-}
+  brandLogged: { name: "HealthCache", to: "/" },
+  linksLogged: [
+    { name: "Profile", to: "/Profile" },
+    { name: "File Claim", to: "/FileClaim" },
+    { name: "Discussion Board", to: "/Discussion" },
+  ],
+};
 const navigationNotLogged = {
-    brand: {name: 'HealthCache', to: '/'},
-    links: [
-        {name: 'Login', to: '/Login'},
-        {name: 'Register', to: '/Register'},
+  brand: { name: "HealthCache", to: "/" },
+  links: [
+    { name: "Login", to: "/Login" },
+    { name: "Register", to: "/Register" },
+    { name: "FileClaim", to: "/FileClaim" },
+  ],
+};
 
-    ]
-}
-
-interface AppProps {
-
-}
+interface AppProps {}
 
 const _App: React.FC<AppProps> = (props) => {
+  const { brandLogged, linksLogged } = navigationLogged;
+  const { brand, links } = navigationNotLogged;
 
-    const {brandLogged, linksLogged} = navigationLogged;
-    const {brand, links} = navigationNotLogged;
+  const appState = useSelector<any, any>((state) => state);
 
-    const appState = useSelector<any, any>((state) => state);
+  return (
+    <div className="App">
+      <Router>
+        {appState.userLogin.user_id === 0 ? (
+          <NavbarScroller brand={brand} links={links} />
+        ) : (
+          <NavbarScroller brand={brandLogged} links={linksLogged} />
+        )}
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/FileClaim" element={<Claims />} />
+          <Route path="/Discussion" element={<Discussions />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
+const mapStateToProps = ({ userLogin }: StoreState): { userLogin: User } => {
+  return { userLogin };
+};
 
-    return (
-        <div className="App">
-            <Router>
-                {appState.userLogin.user_id === 0 ? <NavbarScroller brand={brand} links={links}/>:
-                    <NavbarScroller brand={brandLogged} links={linksLogged}/>}
-                <Routes>
-                    <Route path="/Login" element={<Login/>}/>
-                    <Route path="/Register" element={<Register/>}/>
-                    <Route path="/Profile" element={<Profile/>}/>
-                    <Route path="/FileClaim" element={<Claims/>}/>
-                    <Route path="/Discussion" element={<Discussions/>}/>
-                </Routes>
-            </Router>
-        </div>
-    )
-}
-
-
-const mapStateToProps = ({userLogin}: StoreState): { userLogin: User } => {
-    return {userLogin}
-}
-
-
-export const App = connect(
-    mapStateToProps,
-    {}
-)(_App)
+export const App = connect(mapStateToProps, {})(_App);
--- health-cache-react-frontend/src/components/claims/Claims.tsx
+++ health-cache-react-frontend/src/components/claims/Claims.tsx
@@ -1,28 +1,58 @@
-import { render } from '@testing-library/react';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import './claims.css';
-import { IClaim } from './IClaim';
+import { render } from "@testing-library/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./claims.css";
+import { IClaim } from "./IClaim";
+import {
+  Modal,
+  ModalTitle,
+  Dropdown,
+  DropdownButton,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import { ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
-
-const Claims: React.FC<any> = () => {  
-
+const Claims: React.FC<any> = () => {
   let [claims, setClaims] = useState([]);
-  
+
   const fetchClaims = async () => {
-    let res = await axios.get('http://localhost:8089/claim/all');
+    let res = await axios.get("http://localhost:8089/claim/all");
     setClaims(res.data);
-  }
+  };
 
   useEffect(() => {
     fetchClaims();
-  },[claims.length])
+  }, [claims.length]);
+
+  const [show, setShow] = useState(false);
+  //const [dropval, setDropval] = useState("");
+  // const handleDrop = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log("Changed the value");
+  //   setDropval(event.target.value);
+  // };
+
+  // const handleDrop = (event: React.ChangeEvent<any>) => {
+  //   console.log("Value changed");
+  // };
+
+  // const handleDropSelect = (e: React.SyntheticEvent<unknown>
+  //   ) => {
+  //   console.log("event key", );
+  // };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  console.log(show);
 
   return (
     <div className="content">
       <div className="header-region">
         <h2>My File Claims</h2>
-        <button className="rev-btn" type="button">File New Claim</button>
+        <button className="rev-btn" type="button" onClick={handleShow}>
+          File New Claim
+        </button>
       </div>
       <hr />
       <table>
@@ -32,20 +62,20 @@ const Claims: React.FC<any> = () => {
           <th>Description</th>
           <th>Status</th>
         </tr>
-        {
-          claims.map((claim:IClaim) => {
-            console.log('id: ' + claim.id);
-            console.log('claimType: ' + claim.claimType);
-            console.log('description: ' + claim.description);
-            console.log('status: ' + claim.status);
-            return (<tr>
+        {claims.map((claim: IClaim) => {
+          console.log("id: " + claim.id);
+          console.log("claimType: " + claim.claimType);
+          console.log("description: " + claim.description);
+          console.log("status: " + claim.status);
+          return (
+            <tr>
               <td>{claim.id}</td>
               <td>{claim.claimType}</td>
               <td>{claim.description}</td>
               <td>{claim.status}</td>
-            </tr>);
-          })
-        }
+            </tr>
+          );
+        })}
         {/*<tr>
           <td>1</td>
           <td>Surgery</td>
@@ -65,7 +95,43 @@ const Claims: React.FC<any> = () => {
           <td>DENIED</td>
         </tr>*/}
       </table>
+
+      <Modal show={show}>
+        <ModalHeader>
+          <ModalTitle>Create New File Claim</ModalTitle>
+          <ModalBody>
+            <InputGroup className="mb-3">
+              <DropdownButton
+                variant="outline-secondary"
+                title="Claim Type"
+                onSelect={(
+                  eventKey: string | null,
+                  e: React.SyntheticEvent<unknown>
+                ) => console.log("Event Key ", eventKey)}
+              >
+                <Dropdown.Item eventKey="surgery">SURGERY</Dropdown.Item>
+                <Dropdown.Item eventKey="medication">MEDICATION</Dropdown.Item>
+                <Dropdown.Item eventKey="elective">ELECTIVE</Dropdown.Item>
+                <Dropdown.Item eventKey="emergency">EMERGENCY</Dropdown.Item>
+                <Dropdown.Item eventKey="other">OTHER</Dropdown.Item>
+              </DropdownButton>
+              {/*<FormControl aria-label="Text input with dropdown button" />
+               */}
+            </InputGroup>
+            <InputGroup>
+              <InputGroup.Text>DESCRIPTION</InputGroup.Text>
+              <FormControl as="textarea" aria-label="With textarea" />
+            </InputGroup>
+          </ModalBody>
+          <ModalFooter>
+            <input className="rev-btn" type="submit" value="Submit" />
+            <button className="rev-btn" type="button" onClick={handleClose}>
+              Cancel
+            </button>
+          </ModalFooter>
+        </ModalHeader>
+      </Modal>
     </div>
   );
-}
+};
 export default Claims;
