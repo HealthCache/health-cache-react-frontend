76c68f9a3e9bf15f6b3b13526e6fe8515c3bda43
CharlesSmith98 <charles.w.smith@outlook.com>
2021-11-22 10:19:21 -0500

    Started adding redux for claims

--- health-cache-react-frontend/src/redux/actions/types.ts
+++ health-cache-react-frontend/src/redux/actions/types.ts
@@ -1,13 +1,17 @@
 import {FetchAllUsersAction} from "./users";
 import {LoginUserAction, LogoutUserAction} from "./login";
+import { AddClaimAction, FetchAllClaimsAction, FetchUsersClaimsAction } from "./claims";
 
 export enum ActionTypes{
     fetchaAllUsers,
     login,
     logout,
     register,
+    fetchAllClaims,
+    addClaim,
+    fetchUsersClaims,
 
 }
 
-export type Action = FetchAllUsersAction | LoginUserAction | LogoutUserAction
+export type Action = FetchAllUsersAction | LoginUserAction | LogoutUserAction | FetchAllClaimsAction | AddClaimAction | FetchUsersClaimsAction
     ;
--- health-cache-react-frontend/src/redux/reducers/index.ts
+++ health-cache-react-frontend/src/redux/reducers/index.ts
@@ -1,17 +1,20 @@
 import {combineReducers} from "redux";
 import {usersReducer} from "./users";
 import { User} from "../actions";
+import { Claim } from "../actions/claims";
 import {loginReducer} from "./login";
+import { ClaimsReducer } from "./claims-reducer";
 
 
 export interface StoreState{
     users: User[];
     userLogin: User;
-
+    claims: Claim[];
 
 }
 
 export const reducers = combineReducers<StoreState>({
     users: usersReducer,
     userLogin: loginReducer,
+    claims: ClaimsReducer,
 });
